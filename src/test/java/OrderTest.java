7b9e13811ea8a1200e3d3b0e55392b06f65bb6d9
3113906659 <3113906659@qq.com>
2021-08-04 21:59:45 +0800

    mutation 测试

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -6,6 +6,9 @@ import io.github.shop0.openapi.graphql.api.QueryResolver;
 import io.github.shop0.openapi.graphql.model.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderTest {
     private Shop0Client shop0Client;
 
@@ -37,22 +40,83 @@ public class OrderTest {
     // 关闭订单
     @Test
     public void orderClose() throws Exception {
+        String orderId = "";
+
         MutationResolver mutationResolver = new MutationResolverImpl(shop0Client);
         OrderCloseInputTO input = new OrderCloseInputTO();
-        input.setId("");
+        input.setId(orderId);
 
         OrderCloseTO order = mutationResolver.orderClose(input);
+        System.out.println(order);
+    }
+
+    // 更新订单
+    @Test
+    public void orderUpdate() throws Exception {
+        String orderId = "";
+
+        MutationResolver mutationResolver = new MutationResolverImpl(shop0Client);
+        OrderInputTO input = new OrderInputTO();
+        input.setId(orderId);
+        input.setNote("第1次更新");
+
+        OrderUpdateTO order = mutationResolver.orderUpdate(input);
         System.out.println(order);
     }
 
     // 创建退款单
     @Test
     public void refundCreate() throws Exception {
+        String orderId = "";
+
         MutationResolver mutationResolver = new MutationResolverImpl(shop0Client);
         RefundInputTO input = new RefundInputTO();
-        input.setOrderId("");
+        input.setOrderId(orderId);
 
         RefundCreateTO order = mutationResolver.refundCreate(input);
+        System.out.println(order);
+    }
+
+    // 拒绝退款
+    @Test
+    public void refundSessionReject() throws Exception {
+        String refundSessionId = "";
+
+        MutationResolver mutationResolver = new MutationResolverImpl(shop0Client);
+        RefundSessionRejectionReasonInputTO input = new RefundSessionRejectionReasonInputTO();
+        input.setCode(RefundSessionStatusReasonRejectionCodeTO.PROCESSING_ERROR);
+        input.setMerchantMessage("拒绝退款");
+
+        RefundSessionRejectTO order = mutationResolver.refundSessionReject(refundSessionId, input);
+        System.out.println(order);
+    }
+
+    // 同意退款
+    @Test
+    public void refundSessionResolve() throws Exception {
+        String refundSessionId = "";
+
+        MutationResolver mutationResolver = new MutationResolverImpl(shop0Client);
+
+        RefundSessionResolveTO order = mutationResolver.refundSessionResolve(refundSessionId);
+        System.out.println(order);
+    }
+
+    // 创建物流单
+    @Test
+    public void fulfillmentCreate() throws Exception {
+        String orderId = "";
+
+        MutationResolver mutationResolver = new MutationResolverImpl(shop0Client);
+        FulfillmentV2InputTO input = new FulfillmentV2InputTO();
+        FulfillmentOrderLineItemsInputTO fulfillmentOrderLineItemsInputTO = new FulfillmentOrderLineItemsInputTO();
+        List<FulfillmentOrderLineItemsInputTO> items = new ArrayList<>();
+
+        fulfillmentOrderLineItemsInputTO.setFulfillmentOrderId(orderId);
+        items.add(fulfillmentOrderLineItemsInputTO);
+        input.setLineItemsByFulfillmentOrder(items);
+
+        FulfillmentCreateTO order = mutationResolver.fulfillmentCreate(input, "测试");
         System.out.println(order);
     }
 
