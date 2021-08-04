9b79a00aacea63c71a4fad3e627cc5002e51759d
3113906659 <3113906659@qq.com>
2021-08-04 21:30:47 +0800

    mutation

--- src/main/java/io/github/shop0/openapi/graphql/MutationResolverImpl.java
+++ src/main/java/io/github/shop0/openapi/graphql/MutationResolverImpl.java
@@ -259,9 +259,76 @@ public class MutationResolverImpl implements MutationResolver {
         return proj;
     }
 
+    public FulfillmentResponseProjection getFulfillmentResponseProjection() {
+        FulfillmentResponseProjection proj = new FulfillmentResponseProjection()
+                .id()
+                .fulfillmentLineItems(new FulfillmentLineItemConnectionResponseProjection()
+                        .pageInfo(new PageInfoResponseProjection().all$())
+                        .totalCount()
+                        .edges(new FulfillmentLineItemEdgeResponseProjection()
+                                .cursor()
+                                .node(new FulfillmentLineItemResponseProjection()
+                                        .id()
+                                        .quantity()
+                                        .lineItem(new LineItemResponseProjection()
+                                                .id()
+                                                .image(new ImageResponseProjection()
+                                                        .originalSrc()
+                                                        .transformedSrc()
+                                                )
+                                                .name()
+                                                .product(new ProductResponseProjection()
+                                                        .id()
+                                                        .title()
+                                                )
+                                                .quantity()
+                                                .requiresShipping()
+                                                .sku()
+                                                .title()
+                                                .variant(new ProductVariantResponseProjection()
+                                                        .id()
+                                                )
+                                                .variantTitle()
+                                                .vendor()
+                                                .vendorSku()
+                                        )
+                                )
+                        )
+                )
+                .createdAt()
+                .deliveredAt()
+                .estimatedDeliveryAt()
+                .inTransitAt()
+                .requiresShipping()
+                .service(new FulfillmentServiceResponseProjection()
+                        .handle()
+                        .serviceName()
+                )
+                .status()
+                .totalQuantity()
+                .updatedAt();
+
+        return proj;
+    }
+
     @Override
     public @NotNull CheckoutCreateTO checkoutCreate(@NotNull CheckoutCreateInputTO input) throws Exception {
         return null;
+    }
+
+    @Override
+    public @NotNull FulfillmentCreateTO fulfillmentCreate(@NotNull FulfillmentV2InputTO input, String message) throws Exception {
+        FulfillmentCreateMutationRequest req = FulfillmentCreateMutationRequest.builder().setInput(input).build();
+        FulfillmentCreateResponseProjection proj = new FulfillmentCreateResponseProjection()
+                .userErrors(new UserErrorResponseProjection().all$())
+                .fulfillment(getFulfillmentResponseProjection());
+
+        GraphQLRequest graphQLRequest = new GraphQLRequest(req, proj);
+        FulfillmentCreateMutationResponse result = this.client.invoke(graphQLRequest, FulfillmentCreateMutationResponse.class);
+        if (result.hasErrors()) {
+            throw new Shop0ApiException(result.getErrors());
+        }
+        return result.fulfillmentCreate();
     }
 
     @Override
@@ -280,6 +347,21 @@ public class MutationResolverImpl implements MutationResolver {
     }
 
     @Override
+    public @NotNull OrderUpdateTO orderUpdate(@NotNull OrderInputTO input) throws Exception {
+        OrderUpdateMutationRequest req = OrderUpdateMutationRequest.builder().setInput(input).build();
+        OrderUpdateResponseProjection proj = new OrderUpdateResponseProjection()
+                .userErrors(new UserErrorResponseProjection().all$())
+                .order(getOrderResponseProjection());
+
+        GraphQLRequest graphQLRequest = new GraphQLRequest(req, proj);
+        OrderUpdateMutationResponse result = this.client.invoke(graphQLRequest, OrderUpdateMutationResponse.class);
+        if (result.hasErrors()) {
+            throw new Shop0ApiException(result.getErrors());
+        }
+        return result.orderUpdate();
+    }
+
+    @Override
     public @NotNull RefundCreateTO refundCreate(@NotNull RefundInputTO input) throws Exception {
         RefundCreateMutationRequest req = RefundCreateMutationRequest.builder().setInput(input).build();
         RefundCreateResponseProjection proj = new RefundCreateResponseProjection()
@@ -293,6 +375,36 @@ public class MutationResolverImpl implements MutationResolver {
             throw new Shop0ApiException(result.getErrors());
         }
         return result.refundCreate();
+    }
+
+    @Override
+    public @NotNull RefundSessionRejectTO refundSessionReject(@NotNull String id, @NotNull RefundSessionRejectionReasonInputTO reason) throws Exception {
+        RefundSessionRejectMutationRequest req = RefundSessionRejectMutationRequest.builder().setId(id).setReason(reason).build();
+        RefundSessionRejectResponseProjection proj = new RefundSessionRejectResponseProjection()
+                .userErrors(new RefundSessionRejectUserErrorResponseProjection().all$())
+                .returnSession(new RefundSessionResponseProjection().all$());
+
+        GraphQLRequest graphQLRequest = new GraphQLRequest(req, proj);
+        RefundSessionRejectMutationResponse result = this.client.invoke(graphQLRequest, RefundSessionRejectMutationResponse.class);
+        if (result.hasErrors()) {
+            throw new Shop0ApiException(result.getErrors());
+        }
+        return result.refundSessionReject();
+    }
+
+    @Override
+    public @NotNull RefundSessionResolveTO refundSessionResolve(@NotNull String id) throws Exception {
+        RefundSessionResolveMutationRequest req = RefundSessionResolveMutationRequest.builder().setId(id).build();
+        RefundSessionResolveResponseProjection proj = new RefundSessionResolveResponseProjection()
+                .userErrors(new RefundSessionResolveUserErrorResponseProjection().all$())
+                .returnSession(new RefundSessionResponseProjection().all$());
+
+        GraphQLRequest graphQLRequest = new GraphQLRequest(req, proj);
+        RefundSessionResolveMutationResponse result = this.client.invoke(graphQLRequest, RefundSessionResolveMutationResponse.class);
+        if (result.hasErrors()) {
+            throw new Shop0ApiException(result.getErrors());
+        }
+        return result.refundSessionResolve();
     }
 
     @Override
